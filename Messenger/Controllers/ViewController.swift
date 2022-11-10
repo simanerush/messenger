1f741456ad8352f73a3b394c2e8b53c8bb9a5700
Sima Nerush <2002ssn@gmail.com>
2022-11-09 17:06:58 -0800

    add necessary views

--- Messenger/Controllers/ViewController.swift
+++ Messenger/Controllers/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  Messenger
 //
 //  Created by Sima Nerush on 11/9/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
