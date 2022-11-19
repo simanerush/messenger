d10939031d666ab3383f52de0af0b22d5dafd606
Sima Nerush <2002ssn@gmail.com>
2022-11-19 00:57:03 -0800

    fix crash on register

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -19,7 +19,9 @@ final class DatabaseManager {
 extension DatabaseManager {
   
   public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
-    database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+    var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+    safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+    database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
       guard snapshot.value as? String != nil else {
         completion(false)
         return
@@ -31,7 +33,7 @@ extension DatabaseManager {
   
   /// Inserts new user into database
   public func insertUser(with user: ChatAppUser) {
-    database.child(user.emailAddress).setValue([
+    database.child(user.safeEmail).setValue([
       "first_name": user.firstName,
       "last_name": user.lastName
     ])
@@ -41,4 +43,10 @@ struct ChatAppUser {
   let firstName: String
   let lastName: String
   let emailAddress: String
+  
+  var safeEmail: String {
+    var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+    safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+    return safeEmail
+  }
 }
